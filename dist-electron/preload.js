48ec138863424fa59766e4500290de223e475715
lucas <lucasmessiaspereira322@gmail.com>
2025-08-09 11:24:18 -0300

    Refactor preload.js to improve readability and maintainability
    - Split single line exports into multi-line for clarity
    - Rename parameters for better understanding
    - Add comments for potential future API exposure

--- dist-electron/preload.js
+++ dist-electron/preload.js
@@ -1,1 +1,36 @@
-"use strict";const n=require("electron");n.contextBridge.exposeInMainWorld("electronAPI",{convertVideos:e=>n.ipcRenderer.invoke("convert-videos",e),onProgress:e=>n.ipcRenderer.on("conversion-progress",(o,r)=>e(r)),onConversionStarted:e=>n.ipcRenderer.on("conversion-started",(o,r)=>e(r)),onConversionCompleted:e=>n.ipcRenderer.on("conversion-completed",(o,r)=>e(r)),selectOutputFolder:()=>n.ipcRenderer.invoke("select-output-folder"),windowControl:e=>n.ipcRenderer.send("window-control",e),getCpuCores:()=>n.ipcRenderer.invoke("getCpuCores"),generateThumbnail:e=>n.ipcRenderer.invoke("generate-thumbnail",e),deleteFile:e=>n.ipcRenderer.invoke("delete-file",e)});n.contextBridge.exposeInMainWorld("ipcRenderer",{on(...e){const[o,r]=e;return n.ipcRenderer.on(o,(t,...i)=>r(t,...i))},off(...e){const[o,...r]=e;return n.ipcRenderer.off(o,...r)},send(...e){const[o,...r]=e;return n.ipcRenderer.send(o,...r)},invoke(...e){const[o,...r]=e;return n.ipcRenderer.invoke(o,...r)}});
+"use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("electronAPI", {
+  convertVideos: (options) => electron.ipcRenderer.invoke("convert-videos", options),
+  onProgress: (callback) => electron.ipcRenderer.on("conversion-progress", (_event, data) => callback(data)),
+  onConversionStarted: (callback) => electron.ipcRenderer.on("conversion-started", (_event, data) => callback(data)),
+  onConversionCompleted: (callback) => electron.ipcRenderer.on("conversion-completed", (_event, data) => callback(data)),
+  selectOutputFolder: () => electron.ipcRenderer.invoke("select-output-folder"),
+  windowControl: (action) => electron.ipcRenderer.send("window-control", action),
+  getCpuCores: () => electron.ipcRenderer.invoke("getCpuCores"),
+  generateThumbnail: (videoPath) => electron.ipcRenderer.invoke("generate-thumbnail", videoPath),
+  deleteFile: (filePath) => electron.ipcRenderer.invoke("delete-file", filePath)
+});
+electron.contextBridge.exposeInMainWorld("ipcRenderer", {
+  on(...args) {
+    const [channel, listener] = args;
+    return electron.ipcRenderer.on(
+      channel,
+      (event, ...args2) => listener(event, ...args2)
+    );
+  },
+  off(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.off(channel, ...omit);
+  },
+  send(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.send(channel, ...omit);
+  },
+  invoke(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.invoke(channel, ...omit);
+  }
+  // You can expose other APTs you need here.
+  // ...
+});
